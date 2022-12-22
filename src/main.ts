009922dfbb4e68e9d701b76cc4138df11765af6c
szimeth-jozef <joccopalica@gmail.com>
2022-12-22 15:46:09 +0100

    Add svelte

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './app.css'
+import App from './App.svelte'
+
+// TODO: register service worker for PWA
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("../serviceWorker.js")
+      .then(reg => {
+          console.log("Service worker registered")
+          console.log(reg)
+      })
+      .catch(error => {
+          console.error("Service worker restration failed")
+          console.error(error)
+      })
+}
+else {
+  console.error("Device unable to support this application.")
+}
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
